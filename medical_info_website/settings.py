84663c4bb4973f2c7b9fe5817630da20fe7f2e61
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-08 00:50:03 +0100

    added project's app config to INSTALLED_APPS

--- medical_info_website/settings.py
+++ medical_info_website/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'crispy_forms',
     'widget_tweaks',
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
