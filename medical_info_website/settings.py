6937167050eced0641706673e039d4afb7525995
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-08 00:44:03 +0100

    added installed django tools to INSTALLED_APPS

--- medical_info_website/settings.py
+++ medical_info_website/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
