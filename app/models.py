881fe91bb637430a6068181c8f2a6ff32c49e12e
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-08 01:31:18 +0100

    First object model migration for project's app

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    # Basic information shared between users and medical practitioners
+    
+
+    # User-specific medical information (fields are set to nullable to enable medical practitioners to be persisted on this table also)
+
+    # Flag to differentiate users from medical practitioners
+    pass
