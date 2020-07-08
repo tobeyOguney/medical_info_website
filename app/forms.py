3f5404a8dd8ed8416373de7ab85125f8a5ee1e93
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-08 20:14:02 +0100

    created user signup form

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from django import forms    
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class UserSignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'is_practitioner']
+        labels = {
+            'username': "Username",
+            'first_name': "First Name",
+            'last_name': "Last Name",
+            'email': "Email Address",
+            'is_practitioner', "Medical Practitioner?"
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+        self.fields['is_practitioner'] = True
+        
