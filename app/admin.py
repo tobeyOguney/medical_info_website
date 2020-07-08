1d7478a87c0f99d16b6a9a5696a20ba8f95ea3ad
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-08 03:01:21 +0100

    setup the admin user panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+admin.site.site_header = "Medical Info Website Admin"
+admin.site.site_title = "Medical Info Website Admin Area"
+admin.site.index_title = "Welcome to the medical info website admin area"
+
+admin.site.register(User)
