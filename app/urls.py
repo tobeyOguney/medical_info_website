78402c4272079de7e3a7ea214b1d47cc8b56c47d
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-08 12:22:55 +0100

    created endpoint to recieve user responses

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     # Routes to a page where users can fill in their medical information with relevant questions
     path("users/", views.UserInformationView.as_view(), name="user_information"),
 
+    # An endpoint that recieves the user's response to questions that request for medical information
+    path("users/question/<int:pk>", views.UserResponseSubmitView.as_view(), name="submit_user_response")
+
     # Routes to a page that contains a table which displays all users and their relevant medical records
     path("records/", views.UserRecordsView.as_view(), name="user_records"),
 
