874d84cc4eff4b3357d6590d46520819fde98f58
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-08 12:32:09 +0100

    fixed view relative view imports and a list typo

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,17 @@
-import views
+from .views import UserInformationView, UserRecordsStatisticsView, UserRecordsView, UserResponseSubmitView
 from django.urls import path
 
 app_name = 'app'
 urlpatterns = [
     # Routes to a page where users can fill in their medical information with relevant questions
-    path("users/", views.UserInformationView.as_view(), name="user_information"),
+    path("users/", UserInformationView.as_view(), name="user_information"),
 
     # An endpoint that recieves the user's response to questions that request for medical information
-    path("users/question/<int:pk>", views.UserResponseSubmitView.as_view(), name="submit_user_response")
+    path("users/question/<int:pk>", UserResponseSubmitView.as_view(), name="submit_user_response"),
 
     # Routes to a page that contains a table which displays all users and their relevant medical records
-    path("records/", views.UserRecordsView.as_view(), name="user_records"),
+    path("records/", UserRecordsView.as_view(), name="user_records"),
 
     # Routes to a page that displays the statistical details of the medical records gotten from the users
-    path("statistics/", views.UserRecordStatisticsView.as_view(), name="user_record_statistics"),
+    path("statistics/", UserRecordStatisticsView.as_view(), name="user_record_statistics"),
 ]
