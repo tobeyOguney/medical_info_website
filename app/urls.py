ca05cef83c41e07d79d7645d4b36d9031afece20
tobeyOguney <tobyogunbayo@gmail.com>
2020-07-09 22:30:56 +0100

    added API routes and updated imports

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,7 @@
-from .views import UserInformationView, UserRecordsStatisticsView, UserRecordsView, UserRedirectView
+from .views import (
+    UserInformationView, UserRecordsStatisticsView, UserRecordsView, UserRedirectView, UserListing,
+    get_test_status,
+)
 from django.urls import path
 
 app_name = 'app'
@@ -14,4 +17,7 @@ urlpatterns = [
 
     # Routes to a page that displays the statistical details of the medical records gotten from the users
     path("statistics/", UserRecordsStatisticsView.as_view(), name="user_record_statistics"),
+
+    path("user_listing/", UserListing.as_view(), name = 'user_listing'),
+    path("ajax/test_status/", get_test_status, name = 'get_test_status'),
 ]
